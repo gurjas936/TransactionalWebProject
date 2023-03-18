660098769b5711adb55c2dac9b2b03a65ead6505
Simranjeet213 <107565929+Simranjeet213@users.noreply.github.com>
2023-03-18 00:17:21 -0400

    last added admin

---
+++ Project_electronics/admin_area/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!--bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<!--css file--> 
+
+<link rel="stylesheet" href="../style.css">
+<style>
+    .footer{
+    position:absolute;
+    bottom:0;
+}
+</style>
+
+
+</head>
+<body>
+    <!--Navbar-->
+    <div class="container-fluid p=0">
+        <!--first child-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <div class="container-fluid">
+            <img src="../images/logo.png" alt="" class="logo">
+            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+
+                 <ul class="navbar-nav">
+
+                 <li class="nav-item">
+
+                 <a href="" class="nav-link">Welcome guest</a>
+                 </li>
+                 </ul>
+
+
+             </nav>
+
+             
+
+        </div>
+    </nav>
+    <!--second child--> 
+    <div class="bg-light">
+                <h3 class="text-center p-2">Manage Details</h3>
+             </div>
+
+     <!--third child-->
+     <div class="row">
+        <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+            <div class="p-3">
+
+              <a href="#"><img src="../images/iphone.jpg" alt="" class="admin_image"</a>
+
+              <p class="text-light text-center">Admin Name</p>
+
+            </div>
+
+            <!-------------------------------->
+            <div class="button text-center">
+
+              <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+              <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+            </div>
+        </div>
+     </div>
+     <!--last child-->
+<div class="bg-info p-3 text-center footer">
+    <p>
+        copyright@2023
+    </p>
+</div>
+
+    </div>
+
+
+
+<!--bootstrap js link-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Project_electronics/index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce website using PHP and MySQL</title>
+    <!--Bootstrap CSS link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  
+    <!--font awesome link-->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!--css file-->
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<!--Navbar-->
+<div class="container-fluid p-0">
+    <!--first-child-->
+    <nav class="navbar navbar-expand-lg bg-info">
+  <div class="container-fluid">
+    <img src="./images/logo.png" alt="" class="logo">
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total Price:100</a>
+        </li>
+       
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!--second child-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+<ul class="navbar-nav me-auto">
+
+<li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+</ul>
+
+
+
+
+
+</nav>
+
+<!--third child-->
+<div class="bg-light">
+  <h3 class="text-center">BUY YOUR GADGETS</h3>
+  <p class="text-center">Buy your Electronic products here</p>
+</div>
+
+
+<!--fourth child-->
+<div class="row">
+  <div class="col-md-10">
+   <!--products-->
+   <div class="row">
+    <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/headphone.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+    </div>
+    <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/iphone.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+    </div>
+    <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/laptop.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+    </div>
+    <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/tablet.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+    </div>
+    <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/watch.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+    </div>
+    <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/drone.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-secondary">View More</a>
+  </div>
+</div>
+    </div>
+   </div>
+
+
+  </div>
+  <div class="col-md-2 bg-secondary p-0">
+<!--side nav-->
+
+<!--brands-->
+<ul class="navbar-nav me-auto text-center">
+  <li class="nav-item bg-info">
+    <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Brand1</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Brand2</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Brand3</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Brand4</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Brand5</a>
+  </li>
+</ul>
+
+<!--categories-->
+
+<ul class="navbar-nav me-auto text-center">
+  <li class="nav-item bg-info">
+    <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Category1</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Category2</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Category3</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Category4</a>
+  </li>
+  <li class="nav-item ">
+    <a href="#" class="nav-link text-light">Category5</a>
+  </li>
+</ul>
+
+
+  </div>
+</div>
+
+
+
+
+
+
+
+<!--last child-->
+<div class="bg-info p-3 text-center">
+    <p>
+        copyright@2023
+    </p>
+</div>
+</div>
+
+
+
+
+
+
+
+<!--Bootstrap js link-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</body>
+</html>
